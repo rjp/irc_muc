a168f06934002c43feb1a342c847795f7c68040e
Rob Partington <rjp@backup.frottage.org>
2008-05-30 16:07:23 +0100

    - sketchy irc handling

--- ircd.rb
+++ ircd.rb
@@ -8,5 +8,21 @@ IRC server implemented as an EventMachine eventer
 =end
 
 class Ircd < EventMachine::Connection
+    include EventMachine::Protocols::LineText2
 
+    def crlf(args)
+        self.send_data(*args << "\r\n")
+    end
+
+
+    def post_init()
+        puts "connected"
+# :localhost NOTICE AUTH :BitlBee-IRCd initialized, please go on
+        crlf(":localhost NOTICE AUTH :ircmuc initialised, continue")
+    end
+
+    def receive_line(line)
+        command, *args = line.chomp.gsub(/\r/, '').split(' ')
+        puts "c=[#{command}] a=[#{args.inspect}]"
+    end
 end
