add8d3428f215fb833c05daf45975e0023dcf327
Rob Partington <rjp@backup.frottage.org>
2008-05-30 16:07:03 +0100

    - don't try and stomp on bitlbee - irc is line-oriented so use the linetext2 handler

--- test-em.rb
+++ test-em.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'eventmachine'
-
+require 'protocols/linetext2'
 require 'ircd'
 
 EventMachine::run {
     EventMachine.epoll
-    EventMachine::start_server('0.0.0.0', 6690, Ircd)
+    EventMachine::start_server('0.0.0.0', 6990, Ircd)
     puts "Listening..."
 }
