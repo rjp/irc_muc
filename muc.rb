328b09e9b6ba1756ade0f49d418a4e77462af257
rjp <user@asus-1552752036.(none)>
2008-05-31 01:44:50 +0100

    - mock MUC for testing

--- muc.rb
+++ muc.rb
@@ -4,12 +4,20 @@ require 'xmpp4r/presence'
 require 'yaml'
 
 class Muc
-    attr_accessor :cl, :m
+    attr_accessor :cl, :m, :topic
 
-    def initialize(room, config)
-		@cl = Jabber::Client.new(Jabber::JID.new(config['jid']))
-		@cl.connect
-		@cl.auth(config['password'])
-	    @m = Jabber::MUC::SimpleMUCClient.new(@cl)
+    def initialize(room)
+#		@cl = Jabber::Client.new(Jabber::JID.new(config['jid']))
+#		@cl.connect
+#		@cl.auth(config['password'])
+#	    @m = Jabber::MUC::SimpleMUCClient.new(@cl)
+puts "spawning a muc in the muc"
+		@topic = 'no topic is yet set'
+		return self
     end
+
+	def send(words)
+		puts "->J: #{words}"
+	end
+
 end
