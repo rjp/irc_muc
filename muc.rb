770eea95664649cea22514066b92dfe14005f52d
rjp <user@asus-1552752036.(none)>
2008-06-02 10:18:29 +0100

    - mutex experimentation

--- muc.rb
+++ muc.rb
@@ -2,6 +2,10 @@ require 'xmpp4r'
 require 'xmpp4r/muc/helper/simplemucclient'
 require 'xmpp4r/presence'
 require 'yaml'
+require 'thread'
+
+# how do we synchronise two asynchronous threads, both using callbacks?
+# mutex?
 
 class Muc
     attr_accessor :cl, :m, :topic
@@ -13,6 +17,14 @@ class Muc
 #	    @m = Jabber::MUC::SimpleMUCClient.new(@cl)
 puts "spawning a muc in the muc"
 		@topic = 'no topic is yet set'
+		jmutex = Mutex.new()
+		jmutex.lock()
+		puts "spawning slow thread"
+		Thread.new { sleep 10; jmutex.unlock() }
+		puts "spinning on mutex at #{Time.now}"
+		jmutex.lock()
+		puts "unlocked mutex at #{Time.now}"
+		jmutex.unlock()
 		return self
     end
 
