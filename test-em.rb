68d51aa5d166aae014315ad55ef8344481ec8e02
Rob Partington <zimpenfish@gmail.com>
2008-06-14 00:37:27 +0100

    - avoid bitlbee by default

--- test-em.rb
+++ test-em.rb
@@ -2,16 +2,16 @@ require 'socket'
 require 'ircd'
 
 config = {}
-port = (config['port'] || 6690).to_i
+port = (config['port'] || 6990).to_i
 server = TCPServer.new('localhost', port)
 
 Thread.start(server.accept) do |s|
   	ircd = Ircd.new(s)
-	puts "accepting lines"
 	# TODO move all this to ircd
   	while s.gets do
 		p $_
 		ircd.receive_line($_.chomp.gsub(/\r/,''))
+	    puts "accepting lines"
 	end
 end
 
